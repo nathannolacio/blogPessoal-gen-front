4cbc1e5347689c2b04690fbd432508645c4c090e
nathannolacio <nathannolacio04@gmail.com>
2024-02-20 21:26:38 -0300

    criação das camadas Context, Model, Service

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,26 @@ import NavBar from './components/navBar/NavBar'
 import Footer from './components/footer/Footer'
 import Login from './paginas/login/Login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Cadastrar from './paginas/cadastro/Cadastro'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <NavBar />
-                <div className='min-h-[80vh]'>
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/login' element={<Login />} />
-                        <Route path='/home' element={<Home />} />
-                    </Routes>
-                </div>
-                <Footer />
-            </BrowserRouter>
+            <AuthProvider>
+                <BrowserRouter>
+                    <NavBar />
+                    <div className='min-h-[80vh]'>
+                        <Routes>
+                            <Route path="/" element={<Login />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/cadastro" element={<Cadastrar />} />
+                            <Route path="/home" element={<Home />} />
+                        </Routes>
+                    </div>
+                    <Footer />
+                </BrowserRouter>
+            </AuthProvider>
         </>
     )
 }
